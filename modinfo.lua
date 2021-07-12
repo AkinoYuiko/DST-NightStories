80511343e347653b6a2db17f2adef536be2fd8a2
秋一 <civi@rcg.fun>
2021-07-12 20:11:09 +0800

    update to 1.6.1

--- modinfo.lua
+++ modinfo.lua
@@ -1,16 +1,20 @@
-version = "1.6"
+version = "1.6.1"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = locale == "zh" and "丁香女子学校" or "Civi, Tony, Lssss, kengyou_lei"
 description = locale == "zh" and "[版本: "..version..[[]
 
 更新内容: 
-- 新增一个晨星皮肤（澪专属）
-- 移除澪的提灯皮肤
+- 新增了一部分语言文本.
+
+- 新增一个晨星皮肤（澪专属）.
+- 移除澪的提灯皮肤.
 
 ]] or "[Version: "..version..[[]
 
 Changelog:
+- Added some new strings.
+
 - Added a new item skin for Mio.
 - Removed Lantern skin from Mio.
 
