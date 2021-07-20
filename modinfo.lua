2c98ed1e0facdbd30e4ff7bea949db8531d6a031
秋一 <civi@rcg.fun>
2021-07-20 19:00:09 +0800

    update to 1.7.3

--- modinfo.lua
+++ modinfo.lua
@@ -1,20 +1,22 @@
-version = "1.7.2"
+version = "1.7.3"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = locale == "zh" and "丁香女子学校" or "Civi, Tony, Lssss, kengyou_lei"
 description = locale == "zh" and "[版本: "..version..[[]
 
 更新内容:
-- 更新适配 Glassic API.
+- 更新适配新的 UpvalueHacker.
 
+- 更新适配 Glassic API.
 - 新皮肤高清化, 增加暗黑配色版本.
 - 新增一个蜂王冠皮肤 (素材来自于哈姆雷特, 未高清)
 
 ]] or "[Version: "..version..[[]
 
 Changelog:
-- Update for new version of Glassic API.
+- Update for new UpvalueHacker.
 
+- Update for new version of Glassic API.
 - HD Royal Crown for Bee Queen Crown.
 - Added Dark style version.
 - Added a new skin for Bee Queen Crown (From Hamlet, not HD).
