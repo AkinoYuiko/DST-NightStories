6e67d13cf0c7994a538e523307e843c81bc21430
秋一 <civi@rcg.fun>
2021-08-07 12:16:27 +0800

    Fixed a texture issue.

--- modinfo.lua
+++ modinfo.lua
@@ -1,20 +1,22 @@
-version = "1.11.2"
+version = "1.11.3"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: "..version..[[]
 
 更新内容:
-- 稍微修改了角色动画包.
+- 修复了一个贴图错误.
 
+- 稍微修改了角色动画包.
 - 稍微修改了粒子特效的大小.
 - 给野营灯皮肤新增粒子特效.
 
 ]] or "[Version: "..version..[[]
 
 Changelog:
-- Modified characters' anim pack.
+- Fixed a texture issue.
 
+- Modified characters' anim pack.
 - Increased FX size.
 - Added FX for Camping Lamp.
 
