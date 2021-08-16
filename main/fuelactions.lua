1d564d02bc17a6bfd1052326a33120c5530df6c9
Tony <18810595035@163.com>
2021-08-16 21:43:11 +0800

    Update fuelactions, removed MIOWETFUEL

--- main/fuelactions.lua
+++ main/fuelactions.lua
@@ -1,17 +1,49 @@
-local TUNING = GLOBAL.TUNING
-local FUELTYPE = GLOBAL.FUELTYPE
-local ACTIONS = GLOBAL.ACTIONS
-local Action = GLOBAL.Action
-local ActionHandler = GLOBAL.ActionHandler
+local ENV = env
+GLOBAL.setfenv(1, GLOBAL)
 
 local MIOFUEL = Action({mount_valid=true})
-local MIOWETFUEL = Action({mount_valid=true})
 
 MIOFUEL.id = "MIOFUEL"
-MIOWETFUEL.id = "MIOWETFUEL"
 
-MIOFUEL.str = ACTIONS.ADDFUEL.str
-MIOWETFUEL.str = ACTIONS.ADDWETFUEL.str
+MIOFUEL.stroverridefn = function(act)
+	if act.invobject ~= nil then
+		return act.invobject:GetIsWet() and STRINGS.ACTIONS.ADDWETFUEL or STRINGS.ACTIONS.ADDFUEL
+	end
+end
+
+local function UseFuel(item, target, doer)
+	local wetmult = item:GetIsWet() and TUNING.WET_FUEL_PENALTY or 1
+	local fueled = target.components.fueled
+	if fueled then
+		fueled:DoDelta(item.components.fuel.fuelvalue * fueled.bonusmult * wetmult, doer)
+		-- _d.SoundEmitter:PlaySound("dontstarve/common/nightmareAddFuel")
+		if fueled.ontakefuelfn ~= nil then
+			fueled.ontakefuelfn(target)
+		end
+		return true
+
+	elseif target.components.perishable then
+		target.components.perishable:SetPercent( target.components.perishable:GetPercent() + item.components.fuel.fuelvalue / TUNING.LANTERN_LIGHTTIME * wetmult )
+		return true
+
+	end
+end
+
+MIOFUEL.fn = function(act)
+	if act.doer.components.inventory then
+    	local fuel = act.doer.components.inventory:RemoveItem(act.invobject)
+		if fuel then
+			if UseFuel(fuel, act.target, act.doer) then
+				fuel:Remove()
+				return true
+			else
+				act.doer.components.inventory:GiveItem(fuel)
+			end
+		end
+	end
+end
+
+ENV.AddAction(MIOFUEL)
 
 local function CheckAvailable(target)
 	return target:HasTag("CAVE_fueled") or
@@ -25,49 +57,19 @@ local function CheckAvailable(target)
 		target.prefab == "purpleamulet" 
 end
 
-MIOFUEL.fn = function(act)
-	local _d = act.doer
-	local _t = act.target
-    local fuel = _d.components.inventory and _d.components.inventory:RemoveItem(act.invobject)
-    local wetmult = fuel:GetIsWet() and 0.7 or 1
-    if fuel then
-		if fuel.prefab == "nightmarefuel" and _d.prefab == "miotan" and CheckAvailable(_t) then
-			if _t.components.fueled then
-				_t.components.fueled:DoDelta(fuel.components.fuel.fuelvalue * _t.components.fueled.bonusmult * wetmult )
-				-- _d.SoundEmitter:PlaySound("dontstarve/common/nightmareAddFuel")
-				if _t.components.fueled.ontakefuelfn ~= nil then
-			    	_t.components.fueled.ontakefuelfn(_t)
-			    end
-			elseif _t.components.perishable then
-				_t.components.perishable:SetPercent( _t.components.perishable:GetPercent() + fuel.components.fuel.fuelvalue/TUNING.LANTERN_LIGHTTIME * wetmult )
-			end
-			fuel:Remove()
-			return true
-		else
-			_d.components.inventory:GiveItem(fuel)
-        end
-    end
-end
+ENV.AddComponentAction("USEITEM", "fuel", function(inst, doer, target, actions, right)
+	if doer.prefab == "miotan" and inst.prefab == "nightmarefuel" then
+		if CheckAvailable(target)
+			and (
+				not (doer.replica.rider ~= nil and doer.replica.rider:IsRiding())
+				or (target.replica.inventoryitem ~= nil and target.replica.inventoryitem:IsGrandOwner(doer))
+			) then
 
-MIOWETFUEL.fn = MIOFUEL.fn
-
-AddAction(MIOFUEL)
-AddAction(MIOWETFUEL)
-
-function SetupActionNFuel(inst,doer,target,actions,right)
-	if not (doer.prefab == "miotan" and inst.prefab == "nightmarefuel" ) then return end
-	local _t = target
-	local _add = false
-    if CheckAvailable(_t) and (not (doer.replica.rider ~= nil and doer.replica.rider:IsRiding())
-      or (target.replica.inventoryitem ~= nil and target.replica.inventoryitem:IsGrandOwner(doer))) then
-		table.insert(actions, inst:GetIsWet() and ACTIONS.MIOWETFUEL or ACTIONS.MIOFUEL)
+			table.insert(actions, ACTIONS.MIOFUEL)
+		end
 	end
-end
+end)
 
-AddComponentAction("USEITEM", "fuel", SetupActionNFuel)
-
-AddStategraphActionHandler("wilson", ActionHandler(ACTIONS.MIOFUEL, "doshortaction"))
-AddStategraphActionHandler("wilson_client", ActionHandler(ACTIONS.MIOFUEL, "doshortaction"))
-
-AddStategraphActionHandler("wilson", ActionHandler(ACTIONS.MIOWETFUEL, "doshortaction"))
-AddStategraphActionHandler("wilson_client", ActionHandler(ACTIONS.MIOWETFUEL, "doshortaction"))
+local handler = ActionHandler(ACTIONS.MIOFUEL, "doshortaction")
+ENV.AddStategraphActionHandler("wilson", handler)
+ENV.AddStategraphActionHandler("wilson_client", handler)
