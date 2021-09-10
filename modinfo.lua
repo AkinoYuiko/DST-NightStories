bd839e98e46c97987cb6bd70963301b48a9d7f7d
秋一 <civi@rcg.fun>
2021-09-10 14:29:48 +0800

    update to 1.14.6

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,13 @@
-version = "1.14.5"
+version = "1.14.6"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: "..version..[[]
 
 更新内容:
-- 更新Sanity组件.
+- 移除 Assets 中的"floating_items.zip".
 
+- 更新Sanity组件.
 - 达米捡花/恶魔花不会影响精神值.
 - 更新了部分语言文本.
 - 修复部分皮肤在水上换皮肤丢失模型的问题.
@@ -16,8 +17,9 @@ description = locale == "zh" and "[版本: "..version..[[]
 ]] or "[Version: "..version..[[]
 
 Changelog:
-- Update Sanity component.
+- Removed "floating_items.zip" in assets.
 
+- Update Sanity component.
 - Dummy's sanity will no longer be affected by picking flowers or evil flowers.
 - Update some strings.
 - Fixed issue where body items missing floating anims on reskinned.
