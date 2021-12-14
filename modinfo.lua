ca6ca8a7974eb9dcf9b336ee0588e5903088a254
秋一 <civi@rcg.fun>
2021-12-14 23:33:23 +0800

    Update to 1.8.3

--- modinfo.lua
+++ modinfo.lua
@@ -1,19 +1,23 @@
-version = "1.18.2"
+version = "1.18.3"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: " .. version .. [[]
 
 更新内容:
-- 澪交换燃料采用新的兼容性写法.
+- 所有动作合并到一个文件.
+- 修复黑暗护符充能时声音缺失的问题.
 
+- 澪交换燃料采用新的兼容性写法.
 - 影刀移除容器属性的等待时间增加至2s.
 
 ]] or "[Version: " .. version .. [[]
 
 Changelog:
-- Tweak "tradefuel" to improve compatibility.
+- All actions in one file.
+- Fix missing sound on fueling Dark Amulet.
 
+- Tweak "tradefuel" to improve compatibility.
 - Increase time to remove "container" for Dark Sword to 2s (Orig is 1s).
 ]]
 
