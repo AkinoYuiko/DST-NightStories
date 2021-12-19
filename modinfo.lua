30d090f08a8aa4b5ef794f1c0a9a3c5554276918
秋一 <civi@rcg.fun>
2021-12-20 02:00:54 +0800

    Update to 1.20.4

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,13 @@
-version = "1.20.3"
+version = "1.20.4"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: " .. version .. [[]
 
 更新内容:
-- 修复救赎之心救赎达米时没有触发效果的问题.
+- 修复一个崩溃.
 
+- 修复救赎之心救赎达米时没有触发效果的问题.
 - 澪和达米死亡时会留下一个噩梦燃料，而不是骨架.
 - 修复作祟相关的问题.
 - 达米现在可以作祟活着的澪或者达米.
@@ -14,8 +15,9 @@ description = locale == "zh" and "[版本: " .. version .. [[]
 ]] or "[Version: " .. version .. [[]
 
 Changelog:
-- Fix issue on reviving Dummy.
+- Fix crash on using pocketwatch_revive.
 
+- Fix issue on reviving Dummy.
 - Mio and Dummy leaves a nightmarefuel instead of bone on death.
 - Fix issues with haunt.
 - Dummy is now able to haunt Mio or Dummy.
