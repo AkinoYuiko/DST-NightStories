4df55fa2c61db8958410a10d6d6029052e0badea
秋一 <civi@rcg.fun>
2021-12-22 19:40:12 +0800

    Dummy restores sanity from other

--- main/sanity_rework.lua
+++ main/sanity_rework.lua
@@ -1,38 +1,36 @@
 local AddPrefabPostInit = AddPrefabPostInit
-local AddClassPostConstruct = AddClassPostConstruct
 GLOBAL.setfenv(1, GLOBAL)
 
 -- Dummy Badge --
-AddClassPostConstruct("widgets/statusdisplays", function(self)
-    self.HideDummyBrain = function(self)
-        if self.owner and self.owner.prefab == "dummy" then
-            if self.brain then self.brain:Hide() end
-            if self.moisturemeter then self.moisturemeter:SetPosition(0, -40, 0) end
-        end
+local StatusDisplays = require("widgets/statusdisplays")
+StatusDisplays.HideDummyBrain = function(self)
+    if self.owner and self.owner.prefab == "dummy" then
+        if self.brain then self.brain:Hide() end
+        if self.moisturemeter then self.moisturemeter:SetPosition(0, -40, 0) end
     end
-    self:HideDummyBrain()
+end
+StatusDisplays:HideDummyBrain()
 
-    local SetGhostMode = self.SetGhostMode
-    self.SetGhostMode = function(self, ghostmode)
-        SetGhostMode(self, ghostmode)
-        self:HideDummyBrain()
-    end
-end)
+local SetGhostMode = StatusDisplays.SetGhostMode
+StatusDisplays.SetGhostMode = function(self, ghostmode)
+    SetGhostMode(self, ghostmode)
+    self:HideDummyBrain()
+end
 
 local LIGHT_SANITY_DRAINS =
 {
-	[SANITY_MODE_INSANITY] = {
-		DAY = TUNING.SANITY_DAY_GAIN,
-		NIGHT_LIGHT = TUNING.SANITY_NIGHT_LIGHT,
-		NIGHT_DIM = TUNING.SANITY_NIGHT_MID,
-		NIGHT_DARK = TUNING.SANITY_NIGHT_DARK,
-	},
-	[SANITY_MODE_LUNACY] = {
-		DAY = TUNING.SANITY_LUNACY_DAY_GAIN,
-		NIGHT_LIGHT = TUNING.SANITY_LUNACY_NIGHT_LIGHT,
-		NIGHT_DIM = TUNING.SANITY_LUNACY_NIGHT_MID,
-		NIGHT_DARK = TUNING.SANITY_LUNACY_NIGHT_DARK,
-	},
+    [SANITY_MODE_INSANITY] = {
+        DAY = TUNING.SANITY_DAY_GAIN,
+        NIGHT_LIGHT = TUNING.SANITY_NIGHT_LIGHT,
+        NIGHT_DIM = TUNING.SANITY_NIGHT_MID,
+        NIGHT_DARK = TUNING.SANITY_NIGHT_DARK,
+    },
+    [SANITY_MODE_LUNACY] = {
+        DAY = TUNING.SANITY_LUNACY_DAY_GAIN,
+        NIGHT_LIGHT = TUNING.SANITY_LUNACY_NIGHT_LIGHT,
+        NIGHT_DIM = TUNING.SANITY_LUNACY_NIGHT_MID,
+        NIGHT_DARK = TUNING.SANITY_LUNACY_NIGHT_DARK,
+    },
 }
 
 -- netvars --
@@ -51,25 +49,25 @@ local SourceModifierList = require("util/sourcemodifierlist")
 local Sanity = require("components/sanity")
 Sanity.Recalc = function(self, dt)
     local dapper_delta = 0
-	if self.dapperness_mult ~= 0 then
-		local total_dapperness = self.dapperness
-		for k, v in pairs(self.inst.components.inventory.equipslots) do
+    if self.dapperness_mult ~= 0 then
+        local total_dapperness = self.dapperness
+        for k, v in pairs(self.inst.components.inventory.equipslots) do
             local equippable = v.components.equippable
-            
+
             if equippable ~= nil then
                 local item_dapperness = self.get_equippable_dappernessfn ~= nil and self.get_equippable_dappernessfn(self.inst, equippable) or equippable:GetDapperness(self.inst, self.no_moisture_penalty)
                 total_dapperness = total_dapperness + item_dapperness
             end
-		end
+        end
 
-		total_dapperness = total_dapperness * self.dapperness_mult
-		dapper_delta = total_dapperness * TUNING.SANITY_DAPPERNESS
-	end
+        total_dapperness = total_dapperness * self.dapperness_mult
+        dapper_delta = total_dapperness * TUNING.SANITY_DAPPERNESS
+    end
 
     local moisture_delta = self.no_moisture_penalty and 0 or easing.inSine(self.inst.components.moisture:GetMoisture(), 0, TUNING.MOISTURE_SANITY_PENALTY_MAX, self.inst.components.moisture:GetMaxMoisture())
 
     local light_sanity_drain = LIGHT_SANITY_DRAINS[self.mode]
-	local light_delta = 0
+    local light_delta = 0
 
     if not self.light_drain_immune then
         if TheWorld.state.isday and not TheWorld:HasTag("cave") then
@@ -77,7 +75,7 @@ Sanity.Recalc = function(self, dt)
         else
             local lightval = CanEntitySeeInDark(self.inst) and .9 or self.inst.LightWatcher:GetLightValue()
             light_delta =
-                (   (lightval > TUNING.SANITY_HIGH_LIGHT and light_sanity_drain.NIGHT_LIGHT) or
+                ( (lightval > TUNING.SANITY_HIGH_LIGHT and light_sanity_drain.NIGHT_LIGHT) or
                     (lightval < TUNING.SANITY_LOW_LIGHT and light_sanity_drain.NIGHT_DARK) or
                     light_sanity_drain.NIGHT_DIM
                 ) * self.night_drain_mult
@@ -85,29 +83,29 @@ Sanity.Recalc = function(self, dt)
     end
 
     local aura_delta = 0
-	if not self.sanity_aura_immune then
-		local x, y, z = self.inst.Transform:GetWorldPosition()
-	    local ents = TheSim:FindEntities(x, y, z, TUNING.SANITY_AURA_SEACH_RANGE, SANITYRECALC_MUST_TAGS, SANITYRECALC_CANT_TAGS)
-	    for i, v in ipairs(ents) do
-	        if v.components.sanityaura ~= nil and v ~= self.inst then
+    if not self.sanity_aura_immune then
+        local x, y, z = self.inst.Transform:GetWorldPosition()
+        local ents = TheSim:FindEntities(x, y, z, TUNING.SANITY_AURA_SEACH_RANGE, SANITYRECALC_MUST_TAGS, SANITYRECALC_CANT_TAGS)
+        for i, v in ipairs(ents) do
+            if v.components.sanityaura ~= nil and v ~= self.inst then
                 local is_aura_immune = false
-				if self.sanity_aura_immunities ~= nil then
-					for tag, _ in pairs(self.sanity_aura_immunities) do
-						if v:HasTag(tag) then
-							is_aura_immune = true
-							break
-						end
-					end
-				end
+                if self.sanity_aura_immunities ~= nil then
+                    for tag, _ in pairs(self.sanity_aura_immunities) do
+                        if v:HasTag(tag) then
+                            is_aura_immune = true
+                            break
+                        end
+                    end
+                end
 
                 if not is_aura_immune then
                     local aura_val = v.components.sanityaura:GetAura(self.inst)
-					aura_val = (aura_val < 0 and (self.neg_aura_absorb > 0 and self.neg_aura_absorb * -aura_val or aura_val) * self:GetAuraMultipliers() or aura_val)
-                    -- changed part begin --
+                    aura_val = (aura_val < 0 and (self.neg_aura_absorb > 0 and self.neg_aura_absorb * -aura_val or aura_val) * self:GetAuraMultipliers() or aura_val)
+                    -- Changed Part Start --
                     local _inv = self.inst.components.inventory and self.inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY)
                     local aura_amulet_mult = _inv and (_inv.prefab == "lightamulet" and (aura_val < 0 and 0.5 or 2)) or 1
                     aura_val = aura_val * aura_amulet_mult
-                    -- changed part end --
+                    -- Changed Part End --
                     aura_delta = aura_delta + ((aura_val < 0 and self.neg_aura_immune) and 0 or aura_val)
                 end
             end
@@ -117,12 +115,12 @@ Sanity.Recalc = function(self, dt)
     local mount = self.inst.components.rider:IsRiding() and self.inst.components.rider:GetMount() or nil
     if mount ~= nil and mount.components.sanityaura ~= nil then
         local aura_val = mount.components.sanityaura:GetAura(self.inst)
-		aura_val = (aura_val < 0 and (self.neg_aura_absorb > 0 and self.neg_aura_absorb * -aura_val or aura_val) * self:GetAuraMultipliers() or aura_val)
-        -- changed part begin --
+        aura_val = (aura_val < 0 and (self.neg_aura_absorb > 0 and self.neg_aura_absorb * -aura_val or aura_val) * self:GetAuraMultipliers() or aura_val)
+        -- Changed Part Start --
         local _inv = self.inst.components.inventory and self.inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY)
         local aura_amulet_mult = _inv and (_inv.prefab == "lightamulet" and (aura_val < 0 and 0.5 or 2)) or 1
         aura_val = aura_val * aura_amulet_mult
-        -- changed part end --
+        -- Changed Part End --
         aura_delta = aura_delta + ((aura_val < 0 and self.neg_aura_immune) and 0 or aura_val)
     end
 
@@ -138,7 +136,9 @@ Sanity.Recalc = function(self, dt)
     end
 
     self.rate = self.rate * self.rate_modifier
+    -- Changed Part Start --
     self.inst.replica.sanity:SetRate(self.rate)
+    -- Changed Part End --
 
     self.ratescale =
         (self.rate > .2 and RATE_SCALE.INCREASE_HIGH) or
@@ -196,5 +196,37 @@ SanityReplica.GetIsInducedInsanity = function(self)
         return self.classified.inducedinsanity:value()
     else
         return false
+    end
+end
+
+-- Dummy restores sanity from others --
+local char_list = GetActiveCharacterList()
+for i, char in ipairs(char_list) do
+    if char == "dummy" then
+        char[i] = nil
+    else
+        AddPrefabPostInit(char, function(inst)
+            if not TheWorld.ismastersim then return end
+
+            if not inst.components.sanityaura then
+                inst:AddComponent("sanityaura")
+                inst.components.sanityaura.aura = (TUNING.SANITY_SMALL * 0.15)
+            end
+
+            if inst.components.sanityaura.aurafn then
+                inst.aurafn_prefns = inst.aurafn_prefns or {}
+                inst.aurafn_prefns["dummy"] = inst.components.sanityaura.aurafn
+            end
+
+            local function CalcSanityAura(inst, observer)
+                if observer.prefab == "dummy" then
+                    return inst.components.sanityaura.aura
+                else
+                    return inst.aurafn_prefns["dummy"](inst, observer)
+                end
+            end
+
+            inst.components.sanityaura.aurafn = CalcSanityAura
+        end)
     end
 end
