f3d6863d548ebe144145d8e0a5e6a9440af0f47c
秋一 <civi@rcg.fun>
2021-12-25 20:58:41 +0800

    Update to 1.21.7

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,13 @@
-version = "1.21.6"
+version = "1.21.7"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: " .. version .. [[]
 
 更新内容:
-- 修复烧毁的便携式衣柜可回收的问题.
+- 修复澪和达米死亡时可能产生多个噩梦燃料的问题.
 
+- 修复烧毁的便携式衣柜可回收的问题.
 - 修复了生成pot时包含多余文本的问题.
 - 更新了部分翻译文本.
 - 更新了便携式衣柜的放置间隔.
@@ -16,8 +17,9 @@ description = locale == "zh" and "[版本: " .. version .. [[]
 ]] or "[Version: " .. version .. [[]
 
 Changelog:
-- Fix issue that burnt portable wardrobe is disassemble.
+- Fix issue that Mio or Dummy would leave more than 1 nightmare fuel on death.
 
+- Fix issue that burnt portable wardrobe is disassemble.
 - Fix issue when generating .POT files.
 - Update translate strings.
 - Update deploy spacing for portable wardrobe.
