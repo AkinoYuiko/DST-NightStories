3d72767f25fc80767c077f49aa155a38b91dd5ff
Tony <legendmastertony@gmail.com>
2021-12-25 15:29:18 +0800

    Update version to 1.21

--- modinfo.lua
+++ modinfo.lua
@@ -1,51 +1,17 @@
-version = "1.20.14"
+version = "1.21"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: " .. version .. [[]
 
 更新内容:
-- 修复提灯皮肤存在的问题.
-
-- 更新"sanity_rework".
-- 调整了部分翻译文本.
-- 修复达米不能从其他玩家身上获取San的问题.
-- 调整了部分参数.
-- 优化代码格式.
-- 修复蒜粉可以降低达米精神消耗的问题.
-- 达米传送时的无敌不会保护掉San了.
-- 更新了澪的一部分模型贴图.
-- 修复影灯附近有达米时不强制刷尖嘴的问题.
-- 修复一个崩溃.
-- 修复若干问题.
-- 修复达米在附近时敲远古矿不是必出尖嘴的问题.
-- 修复一个崩溃.
-- 修复救赎之心救赎达米时没有触发效果的问题.
-- 澪和达米死亡时会留下一个噩梦燃料，而不是骨架.
-- 修复作祟相关的问题.
-- 达米现在可以作祟活着的澪或者达米.
+- 新增道具【魔术礼装】(暗影操纵仪解锁).
+- 新增道具【便携式衣柜】(暗影操纵仪解锁).
 ]] or "[Version: " .. version .. [[]
 
 Changelog:
-- Fix issue for Mio's lantern skin.
-
-- Update "sanity_rework".
-- Update some transalte strings.
-- Fix issue that Dummy can not drain sanity from other players.
-- Tweak tuning.
-- Formating codes.
-- Fix Dummy's sanity change affecting by absorption.
-- Dummy will now lose sanity on teleporting.
-- Update Mio's anim tex.
-- Fix nightmare lights not spawning nightmarebeak when nearby Dummy.
-- Fix crash with only_dazui.
-- Fix several issues.
-- Fix not spawning nightmarebeak on statueruins finished when nearby Dummy.
-- Fix crash on using pocketwatch_revive.
-- Fix issue on reviving Dummy.
-- Mio and Dummy leaves a nightmarefuel instead of bone on death.
-- Fix issues with haunt.
-- Dummy is now able to haunt Mio or Dummy.
+- New item "Magic Dress".
+- New item "Portable Wardrobe".
 ]]
 
 forumthread = ""
