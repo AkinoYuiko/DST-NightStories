a2afd5f667db734400226f7ecab4e8d9da2fde5d
秋一 <civi@rcg.fun>
2021-12-30 23:49:57 +0800

    Fix modicon not displayed.

--- modinfo.lua
+++ modinfo.lua
@@ -1,16 +1,21 @@
-version = "1.22.0"
+version = "1.22.1"
+version_compatible = "1.22.0"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: " .. version .. [[]
 
 更新内容:
+- 修复modicon未显示的问题.
+
 - 更新适配纯净辅助。
 
 “黑夜将至，你准备好了吗？”
 ]] or "[Version: " .. version .. [[]
 
 Changelog:
+- Fix modicon not displayed.
+
 - Update for DST-Fixed.
 
 "Night is coming, aren't you ready yet?"
@@ -23,7 +28,7 @@ dont_starve_compatible = false
 reign_of_giants_compatible = false
 all_clients_require_mod = true
 
-icon_atlas = "modicon.xml"
+icon_atlas = "images/modicon.xml"
 icon = "modicon.tex"
 
 priority = 25
