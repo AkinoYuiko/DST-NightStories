687dbe4dd801b1dda0755ce4db77cc9bed897b5d
秋一 <civi@rcg.fun>
2022-01-04 02:51:16 +0800

    Update to 1.23.2

--- modinfo.lua
+++ modinfo.lua
@@ -1,21 +1,22 @@
-version = "1.23.1"
--- version_compatible = "1.23"
+version = "1.23.2"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: " .. version .. [[]
 
 更新内容:
-- 调整了入侵溯源表的动作文本。
+- 修复了澪或达米可以被达米以外的玩家作祟的问题。
 
+- 调整了入侵溯源表的动作文本。
 - 澪和达米现在可以使用噩梦燃料入侵溯源表。
 
 “黑夜将至，你准备好了吗？”
 ]] or "[Version: " .. version .. [[]
 
 Changelog:
-- Change string text of hacking Backtrek Watch.
+- Fix issue that Mio or Dummy can be haunted by non-Dummy players.
 
+- Change string text of hacking Backtrek Watch.
 - Mio and Dummy can hack Backtrek Watch with Nightmare Fuel.
 
 "Night is coming, aren't you ready yet?"
