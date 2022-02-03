03ff66d67dcb19f061c6dedd277a442fe9f6dba9
秋一 <civi@rcg.fun>
2022-02-03 18:38:56 +0800

    Update to 1.24.2

--- modinfo.lua
+++ modinfo.lua
@@ -1,20 +1,22 @@
-version = "1.24.1"
+version = "1.24.2"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: " .. version .. [[]
 
 更新内容:
-- 修复达米被大月灵打死时会被同时治疗的问题。
+- 修复和澪的精神速率有关的问题。
 
+- 修复达米被大月灵打死时会被同时治疗的问题。
 - 澪现在可以使用噩梦燃料充能彩灯。
 
 “黑夜将至，你准备好了吗？”
 ]] or "[Version: " .. version .. [[]
 
 Changelog:
-- Fix Dummy getting healing by gestalts when dying.
+- Fix a bug for Mio's sanity rate.
 
+- Fix Dummy getting healing by gestalts when dying.
 - Mio can refuel Winter Lights.
 
 "Night is coming, aren't you ready yet?"
