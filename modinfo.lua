e5ca46d30b65d64333f48f68ffe68cce73e09c73
秋一 <civi@rcg.fun>
2022-04-08 13:37:11 +0800

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,14 @@
-version = "1.26.5"
+version = "1.26.6"
 -- basic info --
 name = locale == "zh" and "暗夜故事集" or "Night Stories"
 author = "丁香女子学校"
 description = locale == "zh" and "[版本: " .. version .. [[]
 
 更新内容:
-- 修复一些配方问题。
+- Dummy Badge 兼容 Island Adventures 的中毒状态。
+- 调整了暗影破碎枪的耐久计算机制和更新耐久显示的频率。
 
+- 修复一些配方问题。
 - 使用 GlassicAPI.SortBefore 和 GlassicAPI.SortAfter 调整配方排序。
 - 更新配方“灵魂剥离”的动作文本。
 - 更新适配 Glassic API。
@@ -17,8 +19,10 @@ description = locale == "zh" and "[版本: " .. version .. [[]
 ]] or "[Version: " .. version .. [[]
 
 Changelog:
-- Fix issues with recipes.
+- Update DummyBadge for Island Adventures.
+- Update Nightmare Spear's fueled logic.
 
+- Fix issues with recipes.
 - Use GlassicAPI.SortAfter for recipe sorting.
 - Update actionstr for Dummy's recipe.
 - Update for Glassic API.
