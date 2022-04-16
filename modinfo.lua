560941ff79b3a543cccf33065a7fa48f25bb3ba2
秋一 <civi@rcg.fun>
2022-04-16 14:23:02 +0800

    Update loc fn in modinfo.
    Co-authored-by: Tony <Legend-Master@users.noreply.github.com>

--- modinfo.lua
+++ modinfo.lua
@@ -1,23 +1,43 @@
-version = "1.27.1"
+local function loc(t)
+    t.zhr = t.zh
+    t.zht = t.zht or t.zh
+    t.ch = t.ch or t.zh
+    return t[locale] or t.en
+end
+
+local function zh_en(a, b)
+    return loc({
+        zh = a,
+        en = b
+    })
+end
+
+version = "1.27.2"
 -- basic info --
-name = locale == "zh" and "暗夜故事集" or "Night Stories"
-author = "丁香女子学校"
-description = locale == "zh" and "[版本: " .. version .. [[]
+name = zh_en("暗夜故事集", "Night Stories")
+author = zh_en("丁香女子学校", "Civi, Tony, LSSSS")
+description = zh_en(
+    -- zh
+"[版本: " .. version .. [[]
 
 更新内容:
-- 修复澪的自动充能对 Island Adventures 的船装备不生效的问题。
+- 更新 modinfo 的多语言文本功能。
 
+- 修复澪的自动充能对 Island Adventures 的船装备不生效的问题。
 - 使用 GlassicAPI.AddRecipe 改善配方标签管理。
 
-“黑夜将至，你准备好了吗？”
-]] or "[Version: " .. version .. [[]
+“黑夜将至，你准备好了吗？”]],
+    -- en
+"[Version: " .. version .. [[]
 
 Changelog:
+- Update LOC fn in modinfo.
+
 - Fix issue that Mio's auto-refuel not working for boat equiment in Island Adventures.
 - Use GlassicAPI.AddRecipe for better recipe-init.
 
-"Night is coming, aren't you ready yet?"
-]]
+"Night is coming, aren't you ready yet?"]]
+)
 
 forumthread = ""
 api_version = 10
@@ -42,4 +62,7 @@ if not folder_name:find("workshop-") then
     name = name .. " - DEV"
 end
 
-configuration_options = {}
+server_filter_tags = {
+    "night_stories",
+    "night stories",
+}
