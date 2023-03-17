2257aaaffafb6af69ddfe0414e1a074241ee84ce
Civi <civi@rcg.fun>
2023-03-18 05:14:10 +0800

    Increase version to 1.38.4

--- modinfo.lua
+++ modinfo.lua
@@ -2,21 +2,23 @@ local function zheng(a, b)
     return (locale == "zh" or locale == "zht") and a or b
 end
 
-version = "1.38.3"
+version = "1.38.4"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Azur Circle")
 changelog = zheng([[
+- 修复影背包不能用燃料充能的问题。
+
 - 修复注能光明图腾效果错误的问题。
 - 移除一处多余的debug信息。
-
 - 修复一处崩溃。
 - 为Civi重写了食物buff模块。
 - 为澪添加了与纯净恐惧相关的特性。
 ]], [[
+- Fix bug where Night Pack doesn't accept fuel.
+
 - Fix issue where Charged Light Totem has wrong effects.
 - Remove some debug info.
-
 - Fix a crash.
 - Refactor foodbuffs for Civi.
 - Add compatibility for Mio with Pure Horror.
