fcbaa594dfa959bc56078599fecd086a3c2e5153
Civi <civi@rcg.fun>
2023-03-19 01:20:42 +0800

    Fix bug with civi's bonus with foodbuffs.
    Co-authored-by: Tony <Legend-Master@users.noreply.github.com>

--- postinit/prefabs/foodbuffs.lua
+++ postinit/prefabs/foodbuffs.lua
@@ -1,17 +1,47 @@
 local AddPrefabPostInitAny = AddPrefabPostInitAny
 GLOBAL.setfenv(1, GLOBAL)
 
+local Timer = require("components/timer")
+
+local on_save = Timer.OnSave
+function Timer:OnSave(...)
+    local data = on_save(self, ...)
+    if data and data.timers then
+        for k, v in pairs(data.timers) do
+            if self.timers[k] then
+                v.civi_buffed = self.timers[k].civi_buffed
+            end
+        end
+    end
+    return data
+end
+
+local on_load = Timer.OnLoad
+function Timer:OnLoad(data, ...)
+    on_load(self, data, ...)
+    if data and data.timers then
+        for k, v in pairs(data.timers) do
+            if self.timers[k] then
+                self.timers[k].civi_buffed = v.civi_buffed
+            end
+        end
+    end
+end
+
 local function timer_fn(base_fn, buff_timer)
     return function(inst, target)
         FunctionOrValue(base_fn, inst, target)
 
         local duration = inst.components.timer:GetTimeLeft(buff_timer)
 
-        if target and target.prefab == "civi" then
+        if target and target.prefab == "civi" and not inst.components.timer.timers[buff_timer].civi_buffed then
             local level_mult = 1 + target.level * 0.25
 
             inst.components.timer:StopTimer(buff_timer)
             inst.components.timer:StartTimer(buff_timer, duration * level_mult)
+            if inst.components.timer.timers[buff_timer] then
+                inst.components.timer.timers[buff_timer].civi_buffed = true
+            end
         end
     end
 end
