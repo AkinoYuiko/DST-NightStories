95526f7a3a59bb617baa385a1cb02f3dde9724d9
Civi <civi@rcg.fun>
2023-03-20 04:12:16 +0800

    Increase bug to 1.40

--- modinfo.lua
+++ modinfo.lua
@@ -6,26 +6,14 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "1.39.5"
+version = "1.40"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
-- 修复食物BUFF倍率的问题。
-
-- 移除多余的文件。
-- 修复一处代码逻辑问题。
-- 修复影背包缺失图片的问题。
-- 修复一处拼写错误。
-- 从【Glassic API】中迁移了道具和皮肤。
+- 新皮肤：拟真坎普斯背包。
 ]], [[
-- Fix bug with Civi's bonus on foodbuffs.
-
-- Remove some anim.
-- Fix a crash with "Mods In Menu" enabled but mod not enabled in game.
-- Fix missing image for Night Pack.
-- Fix a typo.
-- Merge example items and skins from "Glassic API".
+- New skin: Pantomimed Krampus Sack.
 ]])
 description = zheng("版本: ", "Version: ") .. version ..
     zheng("\n\n更新内容:\n", "\n\nChangelog:\n") .. changelog .. "\n" ..
