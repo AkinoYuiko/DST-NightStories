d09dec82623c40fa0826d76d23c7bc6f7672c0d5
Civi <civi@rcg.fun>
2023-10-07 16:43:55 +0800

    Increase  bug to 1.43.2

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "1.43.1"
+version = "1.43.2"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
