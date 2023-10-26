3fef44e74076751b9a823af638b8317d89267992
Civi <civi@rcg.fun>
2023-10-27 06:26:11 +0800

    Tweak slot UI.

--- main/containerwidgets.lua
+++ main/containerwidgets.lua
@@ -1,42 +1,23 @@
 GLOBAL.setfenv(1, GLOBAL)
 local params = require("containers").params
 
-params.glassiccutter =
-{
-    widget =
-    {
-        slotpos = {
-            Vector3(-2, 25, 0),
-        },
-        animbank = "ui_alterguardianhat_1x1",
-        animbuild = "ui_alterguardianhat_1x1",
-        pos = Vector3(0, 20, 0),
-    },
-    usespecificslotsforitems = true,
-    type = "hand_inv",
-}
-
-params.moonlight_shadow =
-{
-    widget =
-    {
-        slotpos = {
-            Vector3(-2, 25, 0),
-        },
-        animbank = "ui_alterguardianhat_1x1",
-        animbuild = "ui_alterguardianhat_1x1",
-        pos = Vector3(0, 20, 0),
-    },
-    usespecificslotsforitems = true,
-    type = "hand_inv",
-}
-
-local allowed_items = TUNING.MOONLIGHT_SHADOW.BATTERIES
-
-function params.glassiccutter.itemtestfn(container, item, slot)
-    return allowed_items[item.prefab]
-end
+params.moonlight_shadow = deepcopy(params.slingshot)
+params.moonlight_shadow.widget.slotbg = nil
+-- params.moonlight_shadow =
+-- {
+--     widget =
+--     {
+--         slotpos = {
+--             Vector3(-2, 25, 0),
+--         },
+--         animbank = "ui_alterguardianhat_1x1",
+--         animbuild = "ui_alterguardianhat_1x1",
+--         pos = Vector3(0, 20, 0),
+--     },
+--     usespecificslotsforitems = true,
+--     type = "hand_inv",
+-- }
 
 function params.moonlight_shadow.itemtestfn(container, item, slot)
-    return allowed_items[item.prefab]
+    return TUNING.MOONLIGHT_SHADOW.BATTERIES[item.prefab]
 end
