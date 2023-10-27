df714e6336d92dd4752e7cc34a220a38427b9dc1
Civi <civi@rcg.fun>
2023-10-28 07:51:53 +0800

    meme

--- main/containerwidgets.lua
+++ main/containerwidgets.lua
@@ -8,24 +8,8 @@ function params.moonlight_shadow.itemtestfn(container, item, slot)
     return TUNING.MOONLIGHT_SHADOW.BATTERIES[item.prefab]
 end
 
--- params.moonlight_shadow =
--- {
---     widget =
---     {
---         slotpos = {
---             Vector3(-2, 25, 0),
---         },
---         animbank = "ui_alterguardianhat_1x1",
---         animbuild = "ui_alterguardianhat_1x1",
---         pos = Vector3(0, 20, 0),
---     },
---     usespecificslotsforitems = true,
---     type = "hand_inv",
--- }
-
 params.nightsword = deepcopy(params.slingshot)
 params.nightsword.widget.slotbg = nil
-params.nightsword.excludefromcrafting = false
 params.nightsword.acceptsstacks = false
 function params.nightsword.itemtestfn(container, item, slot)
     return item:HasTag("civicrystal")
