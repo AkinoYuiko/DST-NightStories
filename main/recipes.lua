8fef7d114cb6145d7a4517f84fe6199f5dfb42e7
Civi <civi@rcg.fun>
2023-11-06 07:01:41 +0800

    Make Moonlight Shadow recipe no_deconstruction.

--- main/recipes.lua
+++ main/recipes.lua
@@ -103,11 +103,11 @@ SortAfter("blackholestaff", "greenstaff")
 ---------------------
 
 -- 月影（新） --
-AddRecipe("moonlight_shadow", {Ingredient("security_pulse_cage_full", 1), Ingredient("sword_lunarplant", 1), Ingredient("alterguardianhatshard", 1)}, TECH.LUNARFORGING_TWO, {nounlock=true, station_tag="lunar_forge"})
+AddRecipe("moonlight_shadow", {Ingredient("security_pulse_cage_full", 1), Ingredient("sword_lunarplant", 1), Ingredient("alterguardianhatshard", 1)}, TECH.LUNARFORGING_TWO, {nounlock=true, station_tag="lunar_forge", no_deconstruction = true})
 SortAfter("moonlight_shadow", "sword_lunarplant")
 
 -- 恐惧锁链 --
-AddRecipe("horrorchain", {Ingredient("horrorfuel", 4), Ingredient("voidcloth", 2)}, TECH.SHADOWFORGING_TWO, {nounlock=true, station_tag = "shadow_forge"})
+AddRecipe("horrorchain", {Ingredient("horrorfuel", 4), Ingredient("voidcloth", 2)}, TECH.SHADOWFORGING_TWO, {nounlock=true, station_tag = "shadow_forge", no_deconstruction = true})
 SortAfter("horrorchain", "voidcloth_scythe")
 
 -- 武神铥头 --
