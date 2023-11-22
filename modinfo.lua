9f9ec1860283d574ba2ca114b3b915375cc6af50
Civi <civi@rcg.fun>
2023-11-23 04:44:06 +0800

    1.51.4

--- modinfo.lua
+++ modinfo.lua
@@ -6,19 +6,21 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "1.51.3"
+version = "1.51.4"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
-- 新皮肤：莫西鸭魔杖。
+- 修复了几处代码逻辑问题。
 
+- 新皮肤：莫西鸭魔杖。
 - 达米的仪表盘适配酸雨。
 - 月光粉可以保护玩家免收月灵攻击。
 - 新皮肤：麋鹿鸭手杖。
 ]], [[
-- New skin: Mossia Staff.
+- Fix more code logic issue.
 
+- New skin: Mossia Staff.
 - Dummy's Health-Sanity Meter fit for acid rain.
 - Rename: Moonlight Powder -> Lunar Powder.
 - Lunar Powder protects players from gestalt's attack.
