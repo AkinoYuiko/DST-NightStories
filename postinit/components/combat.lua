f2a7162c32e300d760a499842f855a47c0fab6d8
Civi <civi@rcg.fun>
2023-12-02 16:18:08 +0800

    Fix issue where glash triggers chain attack.

--- postinit/components/combat.lua
+++ postinit/components/combat.lua
@@ -1,9 +1,14 @@
 GLOBAL.setfenv(1, GLOBAL)
 
-local GLASH_PREFABS = {
+local DISABLED_WEAPONS =
+{
     glash = true,
-    gflash = true,
-    alterguardianhat_projectile = true,
+    alterguardianhat = true,
+}
+
+local DISABLED_ENTITIES =
+{
+    alterguardianhat_projectile = true
 }
 
 local Combat = require("components/combat")
@@ -19,14 +24,29 @@ local function do_attack_with_check(self, target, weapon, ...)
     end
 end
 
-function Combat:DoAttack(target, weapon, ...)
+local function launching_projectile_testfn(weapon, projectile)
+    return weapon and projectile == nil
+        and (weapon.components.projectile
+            or weapon.components.complexprojectile
+            or weapon.components.weapon:CanRangedAttack())
+end
+
+function Combat:DoAttack(target, weapon, projectile, ...)
     if target == nil then
         target = self.target
     end
 
-    local main_attack = do_attack(self, target, weapon, ...)
+    local main_attack = do_attack(self, target, weapon, projectile, ...)
+ 
+    if launching_projectile_testfn(weapon, projectile) then
+        return main_attack
+    elseif weapon and DISABLED_WEAPONS[weapon.prefab] then
+        return main_attack
+    elseif DISABLED_ENTITIES[self.inst.prefab] then
+        return main_attack
+    end
     
-    if TheWorld.components.horrorchainmanager:HasMember(target) and not GLASH_PREFABS[self.inst.prefab] then -- glash should NOT trigger chain attack.
+    if TheWorld.components.horrorchainmanager:HasMember(target) then
         local ents = TheWorld.components.horrorchainmanager:GetNearbyMembers(target)
         for _, ent in ipairs(ents) do
             if ent ~= target then
@@ -34,7 +54,7 @@ function Combat:DoAttack(target, weapon, ...)
                 self.CanHitTarget = function()
                     return can_hit_target(self, target, weapon)
                 end
-                do_attack_with_check(self, ent, weapon, ...)
+                do_attack_with_check(self, ent, weapon, projectile, ...)
                 self.CanHitTarget = can_hit_target
             end
         end
