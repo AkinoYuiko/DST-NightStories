012fb63ba4b4e2e7bee4ddf53138fdeb86672e97
Civi <civi@rcg.fun>
2023-12-05 18:27:38 +0800

    1.52.6

--- modinfo.lua
+++ modinfo.lua
@@ -6,23 +6,25 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "1.52.5"
+version = "1.52.6"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
-- 修复一个攻击判定问题。
+- 拳击袋显示优化：显示同一帧内受到的伤害总额。
 
 版本更新历史：
+- 修复一个攻击判定问题。
 - 调整了远程武器攻击锁链目标的行为逻辑。
 - 修复启迪之冠在月光粉BUFF期间仍然会触发的问题。
 - 调整了月光粉的代码逻辑。
 - 新增只有拥有技能树的四名角色可以重置洞察。
 - 新增一种快捷重置洞察的方法（详见合成菜单-角色栏）。
 ]], [[
-- Fix issue with attacking rule.
+- Punching Bag shows damage taken in one frame each time.
 
 Version Change Notes:
+- Fix issue with attacking rule.
 - Tweak ranged weapon behaviors on attacking chained target.
 - Fix issue where Enlightened Crown may trigger gestalts when Lunar Powder activated.
 - Tweak code logic for Lunar Powder.
