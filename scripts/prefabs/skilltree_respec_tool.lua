a5b49019ed5e37d8ffcc62fcb4999d6f0d628338
Civi <civi@rcg.fun>
2023-12-20 13:54:52 +0800

    Fix willow missing ondeactivate in her skill tree.

--- scripts/prefabs/skilltree_respec_tool.lua
+++ scripts/prefabs/skilltree_respec_tool.lua
@@ -1,4 +1,4 @@
-local WILSON_BUILDER_TAG =
+local WILSON_SKILL_TAGS =
 {
     "alchemist",
     "gem_alchemistI",
@@ -10,6 +10,18 @@ local WILSON_BUILDER_TAG =
     "ore_alchemistI",
     "ore_alchemistII",
     "ore_alchemistIII",
+}
+
+local WILLOW_SKILL_TAGS =
+{
+    "controlled_burner",
+    "ember_master",
+}
+
+local ALIGN_TAGS =
+{
+    "player_shadow_aligned",
+    "player_luanr_aligned",
 }
 
 local function reset_skill_tree(player)
@@ -26,15 +38,49 @@ local function reset_skill_tree(player)
         end
         skilltreeupdater:SetSkipValidation(prev)
 
-        for _, tag in ipairs(WILSON_BUILDER_TAG) do
+        -- WILSON
+        for _, tag in ipairs(WILSON_SKILL_TAGS) do
             player:RemoveTag(tag)
         end
 
-        player:PushEvent("unlockrecipe")
 
         if player.components.beard then -- for wilson
             player.components.beard:UpdateBeardInventory()
         end
+
+        -- WILLOW :angri:
+        for _, tag in ipairs(WILLOW_SKILL_TAGS) do
+            player:RemoveTag(tag)
+        end
+        for _, tag in ipairs(ALIGN_TAGS) do
+            player:RemoveTag(tag)
+        end
+
+        if player.updatelighters then
+            player:updatelighters()
+        end
+        if player.updateembers then
+            player:updateembers()
+        end
+        if player.bigbernies then
+            for bern, val in pairs(player.bigbernies)do
+                bern:onLeaderChanged(player)
+            end
+        end
+
+        local damagetyperesist = player.components.damagetyperesist
+        if damagetyperesist then
+            damagetyperesist:RemoveResist("lunar_aligned", player, "willow_allegiance_lunar")
+            damagetyperesist:RemoveResist("shadow_aligned", player, "willow_allegiance_shadow")
+        end
+        local damagetypebonus = player.components.damagetypebonus
+        if damagetypebonus then
+            damagetypebonus:RemoveBonus("shadow_aligned", player, "willow_allegiance_lunar")
+            damagetypebonus:RemoveBonus("lunar_aligned", player, "willow_allegiance_shadow")
+        end
+
+        -- Refresh Crafting Menu
+        player:PushEvent("unlockrecipe")
     end
 end
 
