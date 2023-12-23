1ca84d05d393b356036d1e9f1314eb24bee3509c
Civi <civi@rcg.fun>
2023-12-23 14:29:20 +0800

    Fix a typo.

--- postinit/prefabs/gestalt.lua
+++ postinit/prefabs/gestalt.lua
@@ -7,17 +7,19 @@ local GESTALT_PREFABS =
     "gestalt_guard"
 }
 
-for _, prefab in ipairs(GESTALT_PREFABS) do
-    AddPrefabPostInit("gestalt", function(inst)
-        if not TheWorld.ismastersim then return end
+local function postinit(inst)
+    if not TheWorld.ismastersim then return end
 
-        local retarget = inst.components.combat.targetfn
-        inst.components.combat:SetRetargetFunction(1, function(_inst)
-            local target = retarget(inst)
-            if target and target.components.debuffable and target.components.debuffable:HasDebuff("buff_moonglass") then
-                return
-            end
-            return target
-        end)
+    local retarget = inst.components.combat.targetfn
+    inst.components.combat:SetRetargetFunction(1, function(_inst)
+        local target = retarget(inst)
+        if target and target.components.debuffable and target.components.debuffable:HasDebuff("buff_moonglass") then
+            return
+        end
+        return target
     end)
+end
+
+for _, gestalt in ipairs(GESTALT_PREFABS) do
+    AddPrefabPostInit(gestalt, postinit)
 end
