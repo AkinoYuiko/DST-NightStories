4a94f08d04a8e2ec86f2e4e0193716662443bfce
Civi <civi@rcg.fun>
2023-12-23 14:34:01 +0800

    1.53.9

--- modinfo.lua
+++ modinfo.lua
@@ -6,14 +6,15 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "1.53.8"
+version = "1.53.9"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
-- 调整猪王奖励机制。
+- 鸟笼和蚁狮接受整组物品。
 
 最近更新：
+- 调整猪王奖励机制。
 - 猪王接受整组物品。
 - “重置洞察”修复薇洛缺少反激活相关操作的问题（傻逼科雷）。
 - “重置洞察”现已支持薇洛和女武神。
@@ -26,9 +27,10 @@ changelog = zheng([[
 - 新道具：亮茄尖刺球棒。
 - 亮茄尖刺球棒：基础攻击力34，位面伤害17，保质期10天，基础伤害随时间下降。
 ]], [[
-- Tweak rewards by trading with Pig King.
+- Birdcage and Antlion accept whole stack of items.
 
 Recent Changes:
+- Tweak rewards by trading with Pig King.
 - Pig King accepts whole stacks of items.
 - Fix issue that Willow miss "ondeactivate" fn in her skill tree (Fxxk KLEI).
 - Add support for Willow and Wigfrid with "Reset Insights".
