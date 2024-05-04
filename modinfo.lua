248dde7e59b5bba0e9776036565180e5b46e0230
Civi <civi@rcg.fun>
2024-05-04 11:44:47 +0800

    1.55.1

--- modinfo.lua
+++ modinfo.lua
@@ -6,19 +6,27 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "1.55"
+version = "1.55.1"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
+- 调整了澪的自动充能机制。
+
+最近更新：
+- 猪王不再提供【月亮碎片】。
+- 月亮传送门可制作部分配方。
 - 移除配方【亮茄尖刺球棒】。
 - 调整配方【月光粉】。
-- 月亮传送门可制作部分配方。
 - 皮肤适配【姜饼箱妈妈】。
 ]], [[
+- Change how Mio's auto-refueling works.
+
+Recent Changes:
+- Pig King no longer rewards Moon Shards.
+- Set the Celestial Portal as prototyper.
 - Remove Recipe: Brightshade Spike Bat.
 - Tweak Recipe: Lunar Powder.
-- Set the Celestial Portal as prototyper.
 - Bug fixes.
 ]])
 description = zheng("版本: ", "Version: ") .. version ..
