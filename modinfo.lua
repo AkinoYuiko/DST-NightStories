f00ef49068cdec6c54174663edbbff08100b9b98
Civi <civi@rcg.fun>
2024-08-17 12:11:24 +0800

    1.55.7

--- modinfo.lua
+++ modinfo.lua
@@ -6,15 +6,17 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "1.55.6"
+version = "1.55.7"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
-- 修复新更新 (8 月 9 号) 之后的崩溃
-- 重置洞察适配小鱼人和薇诺娜。
+- 提高部分代码的兼容性以适应将来的更新。
+- 强化了【暗影破碎枪】的耐久。
 
 最近更新：
+- 修复新更新 (8 月 9 号) 之后的崩溃
+- 重置洞察适配小鱼人和薇诺娜。
 - 新皮肤【拾荒尖盔】（绝望石头盔）。
 - 新皮肤【幻影石头盔】（绝望石头盔）。
 - 修复皮肤贴图错误：【姜饼箱妈妈】。
@@ -25,10 +27,12 @@ changelog = zheng([[
 - 调整配方【月光粉】。
 - 皮肤适配【姜饼箱妈妈】。
 ]], [[
-- Fix new update (August 9th) crash
-- Wurt and Winona can reset insights now.
+- Improve code compatibility for future updates.
+- Enhance durability of Nightmare Spear.
 
 Recent Changes:
+- Fix new update (August 9th) crash
+- Wurt and Winona can reset insights now.
 - New skin: Scrappy Hatm (Dreadstone Helm).
 - New skin: Phantomstone Helm (Dreadstone Helm).
 - Fix texture issue.
