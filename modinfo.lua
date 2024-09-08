dcd9377f11f979cc4ef9dfbefc9e3145a6bfe372
Civi <civi@rcg.fun>
2024-09-09 04:22:54 +0800

    1.56

--- modinfo.lua
+++ modinfo.lua
@@ -6,44 +6,14 @@ local function zheng(zh, en)
     return LOC[locale] or en
 end
 
-version = "1.55.9"
+version = "1.56"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
-- 尝试修复一处小问题。
-
-最近更新：
-- 提高部分代码的兼容性以适应将来的更新。
-- 强化了【暗影破碎枪】的耐久。
-- 修复新更新 (8 月 9 号) 之后的崩溃
-- 重置洞察适配小鱼人和薇诺娜。
-- 新皮肤【拾荒尖盔】（绝望石头盔）。
-- 新皮肤【幻影石头盔】（绝望石头盔）。
-- 修复皮肤贴图错误：【姜饼箱妈妈】。
-- 调整了澪的自动充能机制。
-- 猪王不再提供【月亮碎片】。
-- 月亮传送门可制作部分配方。
-- 移除配方【亮茄尖刺球棒】。
-- 调整配方【月光粉】。
-- 皮肤适配【姜饼箱妈妈】。
+- 重写了【月影】切换机制。
 ]], [[
-- Try to fix issue with haunting.
-
-Recent Changes:
-- Enhance durability of Nightmare Spear.
-- Improve code compatibility for future updates.
-- Fix new update (August 9th) crash
-- Wurt and Winona can reset insights now.
-- New skin: Scrappy Hatm (Dreadstone Helm).
-- New skin: Phantomstone Helm (Dreadstone Helm).
-- Fix texture issue.
-- Change how Mio's auto-refueling works.
-- Pig King no longer rewards Moon Shards.
-- Set the Celestial Portal as prototyper.
-- Remove Recipe: Brightshade Spike Bat.
-- Tweak Recipe: Lunar Powder.
-- Bug fixes.
+- Rework the way Lunar Shadow switches state.
 ]])
 description = zheng("版本: ", "Version: ") .. version ..
     zheng("\n\n本次更新:\n", "\n\nChanges:\n") .. changelog .. "\n" ..
@@ -69,8 +39,3 @@ folder_name = folder_name or "workshop-"
 if not folder_name:find("workshop-") then
     name = name .. " - DEV"
 end
-
-server_filter_tags = {
-    "night_stories",
-    "night stories",
-}
