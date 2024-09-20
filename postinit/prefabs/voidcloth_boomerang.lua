f34a2cb320d1ca88863c26ce90a5f0e78da1e8bb
Civi <civi@rcg.fun>
2024-09-20 11:17:02 +0800

    Use OnPreHit instead of OnHit.

--- postinit/prefabs/voidcloth_boomerang.lua
+++ postinit/prefabs/voidcloth_boomerang.lua
@@ -7,9 +7,9 @@ end
 
 AddPrefabPostInit("voidcloth_boomerang_proj", function(inst)
     if not TheWorld.ismastersim then return end
-    local onhit = inst.components.projectile.onhit
-    inst.components.projectile:SetOnHitFn(function(_inst, attacker, target, ...)
-        onhit(_inst, attacker, target, ...)
+    local onprehit = inst.components.projectile.onprehit
+    inst.components.projectile:SetOnPreHitFn(function(_inst, attacker, target, ...)
+        FunctionOrValue(onprehit, _inst, attacker, target, ...)
         if target and target:IsValid() and attacker and attacker.prefab == "civi" then
             chain(target, TUNING.CIVI_VOIDCLOTH_BOOMERANG_CHAIN_DURATION)
             local ents = TheWorld.components.horrorchainmanager:GetNearbyMembers(target)
