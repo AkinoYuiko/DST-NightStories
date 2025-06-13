5beb3d9a16eb8ae40e24c1c695a99b350f3a11bc
Civi <civi@rcg.fun>
2025-06-13 17:33:37 +0800

    1.56.9

--- modinfo.lua
+++ modinfo.lua
@@ -6,14 +6,15 @@ local function zheng(zh, en)
 	return LOC[locale] or en
 end
 
-version = "1.56.8"
+version = "1.56.9"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
-- 让达米的血量显示兼容新版本游戏
+- 修复部分皮肤问题
 
 最近更新：
+- 让达米的血量显示兼容新版本游戏
 - 修复奖励数量计算错误的问题。
 - 猪王代码适配最新更新。
 - 调整【月光粉】配方。
@@ -22,9 +23,10 @@ changelog = zheng([[
 - 重写了【月影】切换机制。
 - 修复配方相关的一处错误。
 ]], [[
-- Make dummy's health badge compatible with new version of the game
+- Fix skin issue
 
 Recent Changes:
+- Make dummy's health badge compatible with new version of the game
 - Fix issue that the amount of Pig King's rewards is wrong.
 - Make compatibility with latest release.
 - Tweak recipe for Lunar Powder.
