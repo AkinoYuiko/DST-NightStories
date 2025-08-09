47668d6afa02f54c04da1c114397873cfb1456da
Civi <civi@rcg.fun>
2025-08-09 21:46:07 +0800

    1.57.3

--- modinfo.lua
+++ modinfo.lua
@@ -6,22 +6,26 @@ local function zheng(zh, en)
 	return LOC[locale] or en
 end
 
-version = "1.57.2"
+version = "1.57.3"
 -- basic info --
 name = zheng("暗夜故事集", "Night Stories")
 author = zheng("鸭子乐园", "Ducklantis")
 changelog = zheng([[
-- 启迪之冠的精神回复速率受天体珠宝的数量加成。
+- 收获的季节兼容新版晾肉架。
+- 晾肉架格子接受堆叠。
 
 近期更新：
+- 启迪之冠的精神回复速率受天体珠宝的数量加成。
 - 修复一处计算逻辑错误。
 - 鸟笼兼容官方更新。
 - 暂时禁用了本模组的晾肉架皮肤。
 - 玻璃工具变成可解锁制作了。
 ]], [[
-- Enlightened Crown gains a dapperness multiplier that depends on the amount of Celestial Jewel.
+- The book "Season of Harvest" is now compatible with renewed Meatrack.
+- Meatrack accepts stacking.
 
 Recent changes:
+- Enlightened Crown gains a dapperness multiplier that depends on the amount of Celestial Jewel.
 - Fix a logic issue with calc.
 - Make Bird Cage compatible with recent official update.
 - Temporaryily diable skins for Meatrack.
