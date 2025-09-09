7f0f2df05f85a979044a737fb811f8fa900e9d1d
Tony <legendmastertony@gmail.com>
2025-09-09 21:47:09 +0800

    Use debug.setupvalue instead when setting upvalue

--- postinit/components/brightmarespawner.lua
+++ postinit/components/brightmarespawner.lua
@@ -5,15 +5,14 @@ GLOBAL.setfenv(1, GLOBAL)
 AddComponentPostInit("brightmarespawner", function(self, inst)
 	for _, fn in ipairs(inst.event_listeners["ms_playerjoined"][inst]) do
 		local path = "OnSanityModeChanged.Start.UpdatePopulation.TrySpawnGestaltForPlayer.GetGestaltSpawnType"
-		local get_gestalt_spawn_type = UpvalueUtil.GetUpvalue(fn, path)
+		local get_gestalt_spawn_type, index, scope_fn = UpvalueUtil.GetUpvalue(fn, path)
 		if get_gestalt_spawn_type then
 			print("Hacking:", get_gestalt_spawn_type)
-			local function GetGestaltSpawnType(player, pt, ...)
+			debug.setupvalue(scope_fn, index, function(player, pt, ...)
 				local type = get_gestalt_spawn_type(player, pt, ...)
 				-- print("Gestalt Type:", type)
 				if type == "gestalt_guard_evolved" then
-
-					local _evolved_spawn_pool = UpvalueUtil.GetUpvalue(get_gestalt_spawn_type, "_evolved_spawn_pool")
+					local _evolved_spawn_pool, index, scope_fn = UpvalueUtil.GetUpvalue(self.OnSave, "_evolved_spawn_pool")
 					if player and player.components.inventory and player.components.inventory:EquipHasTag("lunarseedmaxed") then
 						local crown = player.components.inventory.equipslots[EQUIPSLOTS.HEAD]
 						if crown and crown.components.container:Has("lunar_seed", 200) then
@@ -21,15 +20,14 @@ AddComponentPostInit("brightmarespawner", function(self, inst)
 							if type(_evolved_spawn_pool) == "number" then
 								print("_evolved_spawn_pool", _evolved_spawn_pool)
 								_evolved_spawn_pool = _evolved_spawn_pool + 1
-								UpvalueUtil.SetUpvalue(get_gestalt_spawn_type, "_evolved_spawn_pool")
+								debug.setupvalue(scope_fn, index, _evolved_spawn_pool)
 							end
 							print("Gestalt OVERRIDE!")
 						end
 					end
 				end
 				return type
-			end
-			UpvalueUtil.SetUpvalue(fn, path, GetGestaltSpawnType)
+			end)
 			break
 		end
 	end
