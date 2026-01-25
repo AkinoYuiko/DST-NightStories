dc0a917f430d2839e581e46654fb6e9f7787b705
Civi <civi@rcg.fun>
2026-01-25 21:05:51 +0800

    fix: try another way to fix last issue

--- postinit/components/wagpunk_arena_manager.lua
+++ postinit/components/wagpunk_arena_manager.lua
@@ -1,12 +1,13 @@
 -- local AddPrefabPostInit = AddPrefabPostInit
+local AddComponentPostInit = AddComponentPostInit
 ---@diagnostic disable-next-line
 GLOBAL.setfenv(1, GLOBAL)
-
-local WagpunkArenaManager = require("components/wagpunk_arena_manager")
-local add_wagboss_defeated_recipes = WagpunkArenaManager.AddWagbossDefeatedRecipes
-function WagpunkArenaManager:AddWagbossDefeatedRecipes()
-	add_wagboss_defeated_recipes(self)
-	if self.workstation then
-		self.workstation.components.craftingstation:LearnItem("lunarshadow", "lunarshadow")
+AddComponentPostInit("components/wagpunk_arena_manager", function(self)
+	local add_wagboss_defeated_recipes = self.AddWagbossDefeatedRecipes
+	function self:AddWagbossDefeatedRecipes()
+		add_wagboss_defeated_recipes(self)
+		if self.workstation then
+			self.workstation.components.craftingstation:LearnItem("lunarshadow", "lunarshadow")
+		end
 	end
-end
+end)
